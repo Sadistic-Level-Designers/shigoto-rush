5ce0f95b79b20d742d73d63192bb85661392b2ad
James Kerber <kerber.jg@gmail.com>
2022-03-15 13:17:48 +0100

    Placed obstacles

--- Assets/Scripts/NPCmovement.cs
+++ Assets/Scripts/NPCmovement.cs
@@ -7,14 +7,20 @@ public class NPCmovement : MonoBehaviour
      public Vector3 pointA = new Vector3(0, 0, 0);
      public Vector3 pointB = new Vector3(0, 0, 0);
 
-     public float starttime;
-     public float walkduration = 1;
+     public float speed = 0.25f;
      void OnEnable() 
      {
-       starttime = Time.time;
+        timeOffset = Random.Range(0, 10f);
      }
+     private float pt = 0;
+     private float timeOffset = 0;
      void Update()
      {
-         transform.localPosition = Vector3.Lerp(pointA, pointB, Mathf.Clamp01((Time.time-starttime)/walkduration));
+        float t = Mathf.Sin( Mathf.PingPong((Time.time + timeOffset) * speed, 1f) * Mathf.PI/2 );
+         transform.localPosition = Vector3.Lerp(pointA, pointB, t);
+
+
+         transform.localEulerAngles = new Vector3(0f, 180f * ((pt - t) > 0 ? 1f : 0f), 0f);
+         pt = t;
      }
 }
